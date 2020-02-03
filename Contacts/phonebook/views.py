df1a595557ca36d1182b39ce64c2a034c5d0ce80
Shrey Limbani <shreylimbani@gmail.com>
2020-02-03 23:20:45 +0530

    Modified the add page of phonebook application

--- Contacts/phonebook/views.py
+++ Contacts/phonebook/views.py
@@ -5,7 +5,7 @@ from .models import Contact
 
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.all().order_by('firstName')
     #c = Contact.objects.
     return render(request, 'index.html', {'contacts': contacts})
 
