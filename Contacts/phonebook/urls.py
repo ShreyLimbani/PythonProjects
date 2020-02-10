872b40ae55747b6c51d2255d57734c62d334ef2e
Shrey Limbani <shreylimbani@gmail.com>
2020-02-10 23:03:03 +0530

    Made Changes To Add Page

--- Contacts/phonebook/urls.py
+++ Contacts/phonebook/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'phonebook'
 
 urlpatterns = [
-    path('', views.index),
-    path('add.html', views.add)
+    path('', views.index, name='index'),
+    path('new.html', views.new, name='new'),
+    path('new.html', views.add, name='add')
 ]
--- Contacts/phonebook/views.py
+++ Contacts/phonebook/views.py
@@ -6,9 +6,12 @@ from .models import Contact
 # Create your views here.
 def index(request):
     contacts = Contact.objects.all().order_by('firstName')
-    #c = Contact.objects.
     return render(request, 'index.html', {'contacts': contacts})
 
 
+def new(request):
+    return render(request, 'new.html', {'contact': Contact})
+
+
 def add(request):
-    return render(request, 'add.html', {'contact': Contact})
+    return render('phonebook:index')
